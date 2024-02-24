86c7bd0a17cb9b70b3c4abbde4e2f4ed84a5cf7e
haalzi <afis.kian2015@gmail.com>
2024-02-24 23:43:20 +0700

    First Push

---
+++ mdc_100_series/lib/colors.dart
@@ -0,0 +1,31 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color(0xFF343434);
+const kShrinePink100 = Color(0xFF055E68);
+const kShrinePink300 = Color(0xFF62A388);
+const kShrinePink400 = Color(0xFFB9D2D2);
+
+const kShrineBrown900 = Color(0xFFC1C4DA);
+
+const kShrineErrorRed = Color(0xFFE53935);
+
+const kShrineSurfaceWhite = Color(0xFF1F212E);
+const kShrineBackgroundWhite = Color(0xFF1F212E);
+
+const kShrinePurple = Color(0xFFAAC7FF);
+
+
